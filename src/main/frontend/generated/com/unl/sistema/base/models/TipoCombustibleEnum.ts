a011dd9e8d0543f7cd0fa4beb105b26846744b8a
Carlos Josué Granda Cango <carlos.j.granda.c@unl.edu.ec>
2025-06-28 14:11:31 -0500

    Eliminacion de archivos

--- src/main/frontend/generated/com/unl/sistema/base/models/TipoCombustibleEnum.ts
+++
@@ -1,8 +0,0 @@
-enum TipoCombustibleEnum {
-    DIESEL = "DIESEL",
-    ECOPAIS = "ECOPAIS",
-    SUPER = "SUPER",
-    EXTRA = "EXTRA",
-    ELECTRICO = "ELECTRICO"
-}
-export default TipoCombustibleEnum;

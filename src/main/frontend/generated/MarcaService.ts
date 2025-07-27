fc97fc2345e5478a4caf08b9274e30ddfb294e0a
Tayron Morales <mtayronfernando@gmail.com>
2025-07-26 21:51:07 -0500

    Revert "Merge pull request #12 from Carlosjosu/feature/Tayron_ModuloMensajes"
    This reverts commit a2d2ed4440afa9acfebd56da5e18a6b8d20fd33c, reversing
    changes made to d5a65a18d33a6712138b6d2b339fa2f6d907c65b.

--- src/main/frontend/generated/MarcaService.ts
+++
@@ -1,8 +0,0 @@
-import { EndpointRequestInit as EndpointRequestInit_1 } from "@vaadin/hilla-frontend";
-import client_1 from "./connect-client.default.js";
-async function buscarPorAtributo_1(atributo: string | undefined, valor: string | undefined, init?: EndpointRequestInit_1): Promise<Record<string, string | undefined> | undefined> { return client_1.call("MarcaService", "buscarPorAtributo", { atributo, valor }, init); }
-async function create_1(nombre: string | undefined, estaActiva: boolean, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("MarcaService", "create", { nombre, estaActiva }, init); }
-async function listMarca_1(init?: EndpointRequestInit_1): Promise<Array<Record<string, string | undefined> | undefined> | undefined> { return client_1.call("MarcaService", "listMarca", {}, init); }
-async function ordenar_1(atributo: string | undefined, type: number | undefined, init?: EndpointRequestInit_1): Promise<Array<Record<string, string | undefined> | undefined> | undefined> { return client_1.call("MarcaService", "ordenar", { atributo, type }, init); }
-async function update_1(id: number | undefined, nombre: string | undefined, estaActiva: boolean, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("MarcaService", "update", { id, nombre, estaActiva }, init); }
-export { buscarPorAtributo_1 as buscarPorAtributo, create_1 as create, listMarca_1 as listMarca, ordenar_1 as ordenar, update_1 as update };

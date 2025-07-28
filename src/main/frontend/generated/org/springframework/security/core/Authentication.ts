20ab6a93d0cdb5eeba3ca506ebf7607f99ac5599
Tayron Morales <169932075+Tayron2024@users.noreply.github.com>
2025-07-27 20:43:38 -0500

    Revert "Feature/tayron modulo mensajes"

--- src/main/frontend/generated/org/springframework/security/core/Authentication.ts
+++
@@ -1,9 +0,0 @@
-import type GrantedAuthority_1 from "./GrantedAuthority.js";
-interface Authentication {
-    credentials: unknown;
-    principal: unknown;
-    authorities?: Array<GrantedAuthority_1 | undefined>;
-    authenticated: boolean;
-    details: unknown;
-}
-export default Authentication;
